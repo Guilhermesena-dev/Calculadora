70ed9741e1a7511ada70e8e88e8de9ea09cd69a2
Guilhermesena-dev <guiisena2022@gmail.com>
2024-12-11 19:54:36 -0300

    Calculadora_v1

---
+++ testecalculadora.py
@@ -0,0 +1,10 @@
+import calculadora as calc
+def testa_calculadora():
+        print(f"a soma deve dar ={calc.soma(5,3,)}" )
+        print(f"a multiplicação deve dar  ={calc.multiplica(5,3)}")
+        print(f"a divisão deve dar  = {calc.divisão(5,5)}")
+        print(f"a subtração deve dar = {calc.subtrai( 10,9)}")
+
+
+if __name__ == "__main__":
+  testa_calculadora()
