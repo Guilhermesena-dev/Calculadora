cde755276c2d3f12cd9e5d1ef821b87828533b78
Guilhermesena-dev <guiisena2022@gmail.com>
2024-12-11 20:04:07 -0300

    calculadora v_2

--- calculadora.py
+++ calculadora.py
@@ -1,21 +1,34 @@
 
-def soma(a, b,  ):
+acumulador = 0.0 
+
+def soma(a, b = None  ):
+    global acumulador
+    if b == None:
+        return  acumulador + a 
+    else: 
+        acumulador = a + b 
     return a + b 
 
-def subtrai(a,b ):
-    return a - b
-
-def multiplica(a,b  ):
-    return a * b 
-
-def divisão(a,b ):
-    return a*b
-
-def _divisão(a,b ):
-    return a/b 
-
-class acumulador: 
-    def __init__(self, valor_inicial = 0):
-        self.valor = valor_inicial 
+def subtrai(a, b = None ):
+    global acumulador
+    if b == None:
+        return  acumulador -  a 
+    else: 
+        acumulador = a - b 
+    return a + b 
+def multiplica(a, b = None ):
+    global acumulador
+    if b == None:
+        return  acumulador * a 
+    else: 
+        acumulador = a * b 
+    return a + b 
+def divisão(a, b = None ):
+    global acumulador
+    if b == None:
+        return  acumulador / a 
+    else: 
+        acumulador = a  / b 
+    return a + b 
  
 
